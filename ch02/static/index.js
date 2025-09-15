a31f4a60a484436c3f42200b56bf763297bf9b5f
fruitman200912 <fruitman200912@gmail.com>
2025-09-15 20:09:44 +0900

    Update index.js

--- ch02/static/index.js
+++ ch02/static/index.js
@@ -5,7 +5,7 @@ function p() {
   document.getElementById("num").innerText = num;
 }
 
-getElementById("path").addEventListener("click", function() {
+document.getElementById("path").addEventListener("click", function() {
     location.href='http://10.150.0.202:5000/save'+n
     n = 0;
     num.innerHTML = n;
