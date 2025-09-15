33e4abd6f83b9de0492713c489a43588f4ce5c2c
fruitman200912 <fruitman200912@gmail.com>
2025-09-15 20:22:04 +0900

    Update index.py

--- ch02/index.py
+++ ch02/index.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
 session=[]
 
 @app.route("/save/<num>")
@@ -19,13 +16,16 @@ def up(num):
     print(session)
     return render_template("index.html")
 
-@app.route("/save/<num>")
-def up(num):
-    '''
-        1. numcount 테이블 생성(id, num, insert_at)
-        2. pymysql로 연결
-        3. 증가 한 수만큼 추가
-        4. 연결끊기
-        5. db 접속해서 조회
-    '''
-    return render_template("index.html")
+@app.route("/submit", methods=["POST"])
+def save_db():
+    data = request.get_json()
+    print(data.get("value"))
+    conn = pymysql.connetct(host='localhost', user='root', password='q1w2e3', db='study')
+    cur = conn.cursor()
+    cur.execute(f"insert into numcount(num) values({data.get("value")})")
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
